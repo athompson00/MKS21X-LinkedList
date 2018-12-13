a27f440f5a9165a6829d10c4ae756e57df1dbde4
athompson00 <athompson00@stuy.edu>
2018-12-13 11:02:34 -0500

    MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,8 +1,33 @@
+class Node{
+ private int data;
+ private Node next,prev;
+ public Node(int n){
+   data = n;
+   next = null;
+   prev = null;
+ }
+}
+
+
 class MyLinkedList{
  private int size;
  private Node start,end;
 
+ public MyLinkedList(){
+   size = 0;
+   start = null;
+   end = null;
+ }
  public int size();
- public boolean add(int value);
+ public boolean add(int value){
+   Node current = this.start;
+   if (start == null){
+     start = new Node(value);
+   } else {
+     this.end.next = new Node(value);
+     this.end = this.end.next;
+     size++;
+   }
+ }
  public String toString();
 }
