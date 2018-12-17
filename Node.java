15397e489137af35b45662d00578d87a53b7c511
athompson00 <athompson00@stuy.edu>
2018-12-16 20:47:44 -0500

    Node.java

--- Node.java
+++ Node.java
@@ -1,4 +1,28 @@
 class Node{
- private int data;
+ private Integer data;
  private Node next,prev;
+ public Node(Integer n){
+   data = n;
+ }
+ public void setData(Integer n){
+   data = n;
+ }
+ public Node next(){
+   return next;
+ }
+ public Node prev(){
+   return prev;
+ }
+ public void setNext(Node n){
+   next = n;
+ }
+ public void setPrev(Node n){
+   prev = n;
+ }
+ public Integer getData(){
+   return data;
+ }
+ public String toString(){
+   return "" + data;
+ }
 }
